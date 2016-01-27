c79b2105e4ca71c02749ab10db01c9685ea6ccb4
Nathan Lin <nathan.p.lin@gmail.com>
2016-01-26 17:28:11 -0800

    killslider works

--- query.py
+++ query.py
@@ -15,11 +15,31 @@ m_key = 5
 m_meta = 6
 m_count = 7
 
-#cur.execute("SELECT DISTINCT key FROM Minecraft;")
-#pprint.pprint(cur.fetchall())
-#print "\n"
+for i in range(11,15):
+	for j in range(0,8):
+		if j == 0:
+			keyVal = "Logins"
+		elif j == 1:
+			keyVal = "KilledBy"
+		elif j == 2:
+			keyVal = "Chat"
+		elif j == 3:
+			keyVal = "BlockPlace"
+		elif j == 4:
+			keyVal = "Death"
+		elif j == 5:
+			keyVal = "BlockBreak"
+		elif j == 6:
+			keyVal = "Kicks"
+		elif j == 7:
+			keyVal = "CraftedItems"
+		cur.execute("select sum(count) from minecraft where server_id='Server %s' AND key=%s;", (int(i), keyVal))
+		print i
+		print keyVal
+		pprint.pprint(cur.fetchall())
+		print "\n"
 
-cur.execute("\COPY (SELECT * FROM Minecraft WHERE key=' KilledBy') To 'C:\KilledBy.csv' With CSV;")
+#cur.execute("\COPY (SELECT * FROM Minecraft WHERE key=' KilledBy') To 'C:\KilledBy.csv' With CSV;")
 #with open('KilledBy.csv', 'w') as f:
 #	writer = csv.writer(f, delimiter=",")
 #	
@@ -27,4 +47,8 @@ cur.execute("\COPY (SELECT * FROM Minecraft WHERE key=' KilledBy') To 'C:\Killed
 cur.close()
 conn.close()
 
-\copy (select start_t, stop_t, count from minecraft where server_id='server1' AND key='KilledBy') To 'C:\server1kill.csv' With CSV;
+#\copy (select start_t, stop_t, server_id, count from minecraft where key='KilledBy' ORDER BY start_t) To 'C:\serverkills.csv' With CSV;
+
+#select count(*) from (select distinct start_t from minecraft where minecraft.key='Kicks') as test;
+
+#\copy (SELECT DISTINCT start_t, server_id, sum(count) from minecraft where key='KilledBy' GROUP BY start_t, server_id ORDER BY start_t) to 'C:\newkilltest.csv' With CSV;
